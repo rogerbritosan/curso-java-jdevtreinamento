fd608e6a9b6611b80d2e3096123891e4ec497615
Roger <rogerbritosan@gmail.com>
2023-01-19 15:53:31 -0300

    Estrutura de repetição for e continue

---
+++ CursoJavaJdevTreinamento/src/operacoeslogicascondicionais/EstruturaRepeticaoContinue.java
@@ -0,0 +1,20 @@
+package operacoeslogicascondicionais;
+
+public class EstruturaRepeticaoContinue {
+
+	public static void main(String[] args) {
+		
+		// estrutura de repetição for e continue
+		
+		for(int numero = 0; numero <= 10; numero++) {
+			if(numero == 3) {
+				System.out.println("Se liga! Encontrei o número 3");
+				System.out.println("estou pulando...");
+				continue;
+			}
+			System.out.println("Número: " + numero);
+		}
+
+	}
+
+}
