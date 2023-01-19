7feeb709aea4a1bbf54d73d363d21184f9e46716
Roger <rogerbritosan@gmail.com>
2023-01-19 11:42:19 -0300

    Correção das condições

--- CursoJavaJdevTreinamento/src/operacoeslogicascondicionais/OperacoesLogicasIfEElse.java
+++ CursoJavaJdevTreinamento/src/operacoeslogicascondicionais/OperacoesLogicasIfEElse.java
@@ -1,26 +1,24 @@
 package operacoeslogicascondicionais;
 
 public class OperacoesLogicasIfEElse {
-	
+
 	public static void main(String[] args) {
-		
+
 		double nota1 = 10;
 		double nota2 = 9.5;
 		double nota3 = 8;
 		double nota4 = 9;
-		
+
 		double media = (nota1 + nota2 + nota3 + nota4) / 4;
-		
-		if(media >= 7) {
+
+		if (media >= 7) {
 			System.out.println("Aluno aprovado: " + media);
-		}
-		
-		if(media >= 4.9 && media <= 6.9) {
+		} else if (media >= 4.9 && media <= 6.9) {
 			System.out.println("Aluno em recuperação.");
 		} else {
 			System.out.println("Aluno reprovado.");
 		}
-		
+
 	}
 
 }
