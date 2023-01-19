2445da3b5b412baa4b5495b2714c91f8242702c0
Roger <rogerbritosan@gmail.com>
2023-01-19 14:20:01 -0300

    Operadores relacionais

---
+++ CursoJavaJdevTreinamento/src/operacoeslogicascondicionais/OperadoresRelacionais.java
@@ -0,0 +1,18 @@
+package operacoeslogicascondicionais;
+
+public class OperadoresRelacionais {
+
+	public static void main(String[] args) {
+
+		double nota1 = 9.5;
+		double nota2 = 8;
+		
+		if(nota1 > nota2) {
+			System.out.println("Nota 1 é maior que a nota 2");
+		} else {
+			System.out.println("Nota 1 é menor que a nota 2");
+		}
+
+	}
+
+}
