71921b3403d20b851aee82a5213a57ef7e7c7ab8
Roger <rogerbritosan@gmail.com>
2023-01-19 11:48:30 -0300

    Operador ternário

---
+++ CursoJavaJdevTreinamento/src/operacoeslogicascondicionais/OperadorTernarioSimples.java
@@ -0,0 +1,26 @@
+package operacoeslogicascondicionais;
+
+public class OperadorTernarioSimples {
+	
+	public static void main(String[] args) {
+		
+		double nota1 = 10;
+		double nota2 = 9.5;
+		double nota3 = 8;
+		double nota4 = 9;
+
+		double media = (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		// operador ternário
+		
+		String saidaResultado;
+		
+		//saidaResultado = media >=7 ? "Aluno aprovado" : "Aluno reprovado";
+		
+		saidaResultado = media >=7 ? "Aluno aprovado" : (media >= 4.9 && media <= 6.9) ? "Aluno em recuperação" : "Aluno reprovado";
+		
+		System.out.println(saidaResultado);
+		
+	}
+
+}
