b36d0cae79ea9df07657e69db1caca83fee41135
Roger <rogerbritosan@gmail.com>
2023-01-19 17:06:50 -0300

    Criação dos getters and setters

--- CursoJavaJdevTreinamento/src/cursojava/classes/Aluno.java
+++ CursoJavaJdevTreinamento/src/cursojava/classes/Aluno.java
@@ -2,29 +2,110 @@ package cursojava.classes;
 
 public class Aluno {
 
-	public String nome;
-	int idade;
-	String dataNascimento;
-	String registroGeral;
-	String numeroCpf;
-	String nomeMae;
-	String nomePai;
-	String dataMatricula;
-	String nomeEscola;
-	String serieMatriculado;
-	
+	private String nome;
+	private int idade;
+	private String dataNascimento;
+	private String registroGeral;
+	private String numeroCpf;
+	private String nomeMae;
+	private String nomePai;
+	private String dataMatricula;
+	private String nomeEscola;
+	private String serieMatriculado;
+
 	// construtor padrão
 	public Aluno() {
-		
+
 	}
-	
+
 	public Aluno(String nome) {
 		this.nome = nome;
 	}
-	
+
 	public Aluno(String nome, int idade) {
 		this.nome = nome;
 		this.idade = idade;
+	}
+
+	// getters and setters
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return this.idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getDataNascimento() {
+		return this.dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
 	}
 
 }
--- CursoJavaJdevTreinamento/src/cursojava/executavel/TesteAluno.java
+++ CursoJavaJdevTreinamento/src/cursojava/executavel/TesteAluno.java
@@ -7,11 +7,22 @@ public class TesteAluno {
 	public static void main(String[] args) {
 		
 		Aluno aluno1 = new Aluno();
-		aluno1.nome = "Rogério";
+		aluno1.setNome("Rogério");
+		aluno1.setIdade(39);
+		aluno1.setDataNascimento("28/01/1983");
 		
-		System.out.println(aluno1.nome); 
+		System.out.println("Nome: " + aluno1.getNome());
+		System.out.println("Idade: " + aluno1.getIdade());
+		System.out.println("Data de nasc: " + aluno1.getDataNascimento());
 		
 		Aluno aluno2 = new Aluno();
+		aluno2.setNome("Brito");
+		aluno2.setIdade(25);
+		aluno2.setDataNascimento("01/10/1980");
+		
+		System.out.println("Nome: " + aluno2.getNome());
+		System.out.println("Idade: " + aluno2.getIdade());
+		System.out.println("Data de nasc: " + aluno2.getDataNascimento());
 		
 		Aluno aluno3 = new Aluno();
 		
