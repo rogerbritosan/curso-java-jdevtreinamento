503c6f2d8dc8223150f26a5f821d51c48b9b81cf
Roger <rogerbritosan@gmail.com>
2023-01-19 15:42:54 -0300

    Estrutura de repetição for

---
+++ CursoJavaJdevTreinamento/src/operacoeslogicascondicionais/RepeticaoFor.java
@@ -0,0 +1,20 @@
+package operacoeslogicascondicionais;
+
+public class RepeticaoFor {
+
+	public static void main(String[] args) {
+		
+		// estrutura de repetição for
+		for(int numero = 0; numero <= 10; numero++) {
+			System.out.println("Número atual é " + numero);
+		}
+		
+		System.out.println("-------------------------");
+		
+		for(int numero = 10; numero >= 0; numero--) {
+			System.out.println("Número atual é " + numero);
+		}
+
+	}
+
+}
---
+++ CursoJavaJdevTreinamento/src/operacoeslogicascondicionais/RepeticaoWhileDoWhile.java
@@ -0,0 +1,25 @@
+package operacoeslogicascondicionais;
+
+public class RepeticaoWhileDoWhile {
+
+	public static void main(String[] args) {
+
+		// estrutura de repetição
+		int numero = 1;
+		while (numero <= 60) {
+			System.out.println("O número atual é: " + numero);
+			numero++;
+		}
+		
+		System.out.println("-------------------------------------");
+
+		// primeiro executa e depois verifica
+		int x = 1;
+		do {
+			System.out.println("O número atual é: " + x);
+			x++;
+		} while (x <= 30);
+
+	}
+
+}
