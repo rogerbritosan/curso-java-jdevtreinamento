43ff889ec311722834da829a927361737e865c7e
Roger <rogerbritosan@gmail.com>
2023-01-19 15:45:48 -0300

    Estrutura de repetição for com parada forçada

---
+++ CursoJavaJdevTreinamento/src/operacoeslogicascondicionais/RepeticaoForBreak.java
@@ -0,0 +1,19 @@
+package operacoeslogicascondicionais;
+
+public class RepeticaoForBreak {
+
+	public static void main(String[] args) {
+		
+		// estrutura de repetição for (com parada)
+		
+		for(int numero = 0; numero <= 10; numero++) {
+			if(numero == 3) {
+				System.out.println("Encontrei o número três!");
+				System.out.println("Vou parar a execução...");
+				break;
+			}
+		}
+
+	}
+
+}
