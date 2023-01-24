7b47263791bd4781106351d94c097cc912c64715
Roger <rogerbritosan@gmail.com>
2023-01-24 11:02:35 -0300

    Recuperando a nota de cada bimestre de cada disciplina, pel array

--- CursoJavaJdevTreinamento/src/cursojava/executavel/ArrayVetor4.java
+++ CursoJavaJdevTreinamento/src/cursojava/executavel/ArrayVetor4.java
@@ -9,39 +9,42 @@ public class ArrayVetor4 {
 
 	public static void main(String[] args) {
 
-		// 13.6 - Adicionando o array de notas dentro da disciplina
+		// 13.7 - Percorrendo listas e arrays
 
 		Aluno aluno = new Aluno();
 		aluno.setNome("Rogério Brito dos Santos");
 		aluno.setNomeEscola("Jdev Treinamento");
 
-		double[] notas = {8.9, 10, 9.5, 7};
-		double[] notasHTML = {7.9, 8, 7.5, 10};
-		
+		double[] notas = { 8.9, 10, 9.5, 7 };
+		double[] notasHTML = { 7.9, 8, 7.5, 10 };
+
 		Disciplina disciplina = new Disciplina();
-		disciplina.setDisciplina("Java");		
+		disciplina.setDisciplina("Java");
 		disciplina.setNota(notas);
-		
+
 		Disciplina disciplina2 = new Disciplina();
-		disciplina2.setDisciplina("HTML");		
+		disciplina2.setDisciplina("HTML");
 		disciplina2.setNota(notasHTML);
-		
-		
+
 		aluno.getDisciplinas().add(disciplina);
 		aluno.getDisciplinas().add(disciplina2);
-		
+
 		System.out.println("Nome do aluno: " + aluno.getNome() + " inscrito no curso: " + aluno.getNomeEscola());
-		for(Disciplina d : aluno.getDisciplinas()) {
+		System.out.println();
+
+		for (Disciplina d : aluno.getDisciplinas()) {
+
 			System.out.println("Disciplina: " + d.getDisciplina());
 			System.out.println("Notas das disciplinas: ");
-			
-			for(int pos = 0; pos < d.getNota().length; pos++) {
+
+			for (int pos = 0; pos < d.getNota().length; pos++) {
 				System.out.println("Nota: " + pos + " é igual = " + d.getNota()[pos]);
 			}
-			
+
+			System.out.println();
+
 		}
-		
-		
+
 	}
 
 }
