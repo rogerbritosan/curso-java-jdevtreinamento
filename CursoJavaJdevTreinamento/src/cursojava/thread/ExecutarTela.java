9886196d6715fe4c1442e231e4cbf4318dda6a61
Roger <rogerbritosan@gmail.com>
2023-01-25 11:53:34 -0300

    Controlando a Thead 1 com Start e Stop

---
+++ CursoJavaJdevTreinamento/src/cursojava/thread/ExecutarTela.java
@@ -0,0 +1,15 @@
+package cursojava.thread;
+
+public class ExecutarTela {
+
+	public static void main(String[] args) {
+		
+		// 14.6 Criando um tela gráfica com JDialog
+		// 14.8 - Adicionando os botões JButton
+		// 14.9 - Controlando a Thead 1 com Start e Stop
+		
+		TelaTimeThread tela = new TelaTimeThread();
+
+	}
+
+}
---
+++ CursoJavaJdevTreinamento/src/cursojava/thread/TelaTimeThread.java
@@ -0,0 +1,131 @@
+package cursojava.thread;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class TelaTimeThread extends JDialog {
+
+	private JPanel jPanel = new JPanel(new GridBagLayout()); // painel de components
+	private JLabel descricaoHora = new JLabel("Time Thread 1");
+	private JTextField mostraTempo = new JTextField();
+	private JLabel descricaoHora2 = new JLabel("Time Thred 2");
+	private JTextField mostraTempo2 = new JTextField();
+
+	private JButton jButton = new JButton("Start");
+	private JButton jButton2 = new JButton("Stop");
+
+	private Runnable thread1 = new Runnable() {
+
+		public void run() {
+			while (true) { // fica sempre rodando
+				mostraTempo
+						.setText(new SimpleDateFormat("dd/MM/yyyy hh:mm.ss").format(Calendar.getInstance().getTime()));
+
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+
+	};
+
+	private Thread thread1Time;
+
+	public TelaTimeThread() {
+
+		// 14.6 Criando um tela gráfica com JDialog
+		// 14.8 - Adicionando os botões JButton
+		// 14.9 - Controlando a Thead 1 com Start e Stop
+
+		// executa o que estiver dentro
+		setTitle("Minha tela de time com Thread");
+
+		// tamanho
+		setSize(new Dimension(240, 240));
+
+		// posicionamento ao centro
+		setLocationRelativeTo(null);
+
+		// não redimensionar a tela
+		setResizable(false);
+
+		// controlador de posicionamento de elementos da tela
+		GridBagConstraints gridBagConstraints = new GridBagConstraints();
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 0;
+		gridBagConstraints.gridwidth = 2;
+		gridBagConstraints.insets = new Insets(5, 10, 5, 5);
+		gridBagConstraints.anchor = GridBagConstraints.WEST;
+
+		descricaoHora.setPreferredSize(new Dimension(200, 25));
+		jPanel.add(descricaoHora, gridBagConstraints);
+
+		mostraTempo.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy++;
+		mostraTempo.setEditable(false);
+		jPanel.add(mostraTempo, gridBagConstraints);
+
+		descricaoHora2.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy++;
+		jPanel.add(descricaoHora2, gridBagConstraints);
+
+		mostraTempo2.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy++;
+		mostraTempo2.setEditable(false);
+		jPanel.add(mostraTempo2, gridBagConstraints);
+
+		gridBagConstraints.gridwidth = 1;
+
+		jButton.setPreferredSize(new Dimension(92, 25));
+		gridBagConstraints.gridy++;
+		jPanel.add(jButton, gridBagConstraints);
+
+		jButton2.setPreferredSize(new Dimension(92, 25));
+		gridBagConstraints.gridx++;
+		jPanel.add(jButton2, gridBagConstraints);
+
+		jButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) { // executa o clique no botão
+
+				thread1Time = new Thread(thread1);
+				thread1Time.start();
+
+			}
+		});
+
+		jButton2.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				thread1Time.stop();
+
+			}
+		});
+
+		add(jPanel, BorderLayout.WEST); // alinhamento à esquerda
+
+		// sempre será o último comando
+		setVisible(true);
+
+	}
+
+}
